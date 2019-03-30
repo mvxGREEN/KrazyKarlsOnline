c6d8d0e1d0edb73e570e7fc040af7622fc7df728
Mvx Green <maxwellgreen24@gmail.com>
2019-03-30 13:46:09 -0600

    Organize pizza enum, add method for generating base crust price

--- app/src/main/java/com/xxxgreen/mvx/krazykarlsonline/data/parcels/Pizza.java
+++ app/src/main/java/com/xxxgreen/mvx/krazykarlsonline/data/parcels/Pizza.java
@@ -1,13 +1,13 @@
 package com.xxxgreen.mvx.krazykarlsonline.data.parcels;
 
 import android.database.Cursor;
+import android.util.Log;
 
 public class Pizza extends ItemEntree {
-    public enum SIZE {
-        GRINDER, SMALL, MEDIUM, LARGE,
-    }
-    public enum MODIFIER {
-        THIN, GLUTEN_FREE
+    private static final String TAG = "ItemEntree (Pizza)";
+
+    public enum TYPE {
+        GRINDER, SMALL, SMALL_THIN, MEDIUM, MEDIUM_THIN, MEDIUM_GF, LARGE, LARGE_THIN
     }
 
     public Pizza(String name, String base, String top1, String top2, String top3,
@@ -17,5 +17,29 @@ public class Pizza extends ItemEntree {
 
     public Pizza(Cursor cursor) {
         super(cursor);
+    }
+
+    public double getPrice(Pizza.TYPE type) {
+        double price = 0.00;
+        switch (type) {
+            case GRINDER:
+            case SMALL:
+            case SMALL_THIN:
+                price = 4.99;
+                break;
+            case MEDIUM:
+            case MEDIUM_THIN:
+                price = 5.99;
+                break;
+            case MEDIUM_GF:
+                price = 11.99;
+                break;
+            case LARGE:
+            case LARGE_THIN:
+                price = 7.99;
+                break;
+        }
+        Log.i(TAG, "Generated price: " + price);
+        return price;
     }
 }
