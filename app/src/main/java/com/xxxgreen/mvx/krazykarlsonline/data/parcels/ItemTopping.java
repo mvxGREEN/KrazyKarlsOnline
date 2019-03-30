f6dfc9aec4f0d161ea907245b0a9f9898ee41a98
Mvx Green <maxwellgreen24@gmail.com>
2019-03-30 14:30:50 -0600

    Add topping item class (parcelable)

---
+++ app/src/main/java/com/xxxgreen/mvx/krazykarlsonline/data/parcels/ItemTopping.java
@@ -0,0 +1,67 @@
+package com.xxxgreen.mvx.krazykarlsonline.data.parcels;
+
+
+import android.database.Cursor;
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by MVX on 12/22/2018.
+ */
+
+public class ItemTopping implements Parcelable {
+    public final int id;
+    public final String name, types, notes;
+
+    public ItemTopping(String name, String types, String notes) {
+        this.id = 0;
+        this.name = name;
+        this.types = types;
+        this.notes = notes;
+    }
+
+    public ItemTopping(Cursor cursor) {
+        this.id = cursor.getInt(0);
+        this.name = cursor.getString(1);
+        this.types = cursor.getString(2);
+        this.notes = cursor.getString(3);
+    }
+
+    public ItemTopping(Parcel in) {
+        this.id = in.readInt();
+        this.name = in.readString();
+        this.types = in.readString();
+        this.notes = in.readString();
+    }
+
+    public static final Creator<ItemTopping> CREATOR = new Creator<ItemTopping>() {
+        @Override
+        public ItemTopping createFromParcel(Parcel in) {
+            return new ItemTopping(in);
+        }
+
+        @Override
+        public ItemTopping[] newArray(int size) {
+            return new ItemTopping[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.name);
+        dest.writeString(this.types);
+        dest.writeString(this.notes);
+    }
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
+
+}
--- app/src/main/java/com/xxxgreen/mvx/krazykarlsonline/data/parcels/Pizza.java
+++ app/src/main/java/com/xxxgreen/mvx/krazykarlsonline/data/parcels/Pizza.java
@@ -1,13 +1,21 @@
 package com.xxxgreen.mvx.krazykarlsonline.data.parcels;
 
 import android.database.Cursor;
+import android.support.annotation.Nullable;
 import android.util.Log;
+
+import java.util.ArrayList;
+
+import static com.xxxgreen.mvx.krazykarlsonline.data.parcels.Pizza.PRICE_MOD.SIGNATURE;
 
 public class Pizza extends ItemEntree {
     private static final String TAG = "ItemEntree (Pizza)";
 
-    public enum TYPE {
+    public enum CRUST {
         GRINDER, SMALL, SMALL_THIN, MEDIUM, MEDIUM_THIN, MEDIUM_GF, LARGE, LARGE_THIN
+    }
+    public enum PRICE_MOD {
+        BUILD_YOUR_OWN, SIGNATURE, BEAT_THE_CLOCK
     }
 
     public Pizza(String name, String base, String top1, String top2, String top3,
@@ -19,27 +27,66 @@ public class Pizza extends ItemEntree {
         super(cursor);
     }
 
-    public double getPrice(Pizza.TYPE type) {
-        double price = 0.00;
-        switch (type) {
-            case GRINDER:
-            case SMALL:
-            case SMALL_THIN:
-                price = 4.99;
-                break;
-            case MEDIUM:
-            case MEDIUM_THIN:
-                price = 5.99;
-                break;
-            case MEDIUM_GF:
-                price = 11.99;
-                break;
-            case LARGE:
-            case LARGE_THIN:
-                price = 7.99;
-                break;
+    /*
+     *  Params:
+     *      - Crust
+     *      - Toppings
+     *      - Price Modifier
+     *      - Modified Toppings (Nullable)
+     *
+     */
+    public double getCrustPrice(Pizza.CRUST crust, ArrayList<String> toppings,
+                        Pizza.PRICE_MOD mod, @Nullable ArrayList<String> modToppings) {
+        double crustPrice = 0.00;
+
+        if (mod == SIGNATURE) {
+            // Signature price
+            switch (crust) {
+                case GRINDER:
+                    crustPrice = 7.99;
+                case SMALL:
+                case SMALL_THIN:
+                    crustPrice = 8.99;
+                    break;
+                case MEDIUM:
+                case MEDIUM_THIN:
+                    crustPrice = 5.99;
+                    break;
+                case MEDIUM_GF:
+                    crustPrice = 11.99;
+                    break;
+                case LARGE:
+                case LARGE_THIN:
+                    crustPrice = 7.99;
+                    break;
+            }
+        } else {
+            // Base crust price
+            switch (crust) {
+                case GRINDER:
+                    //TODO grinder BYcheck for accuracy
+                    crustPrice = 3.99;
+                    break;
+                case SMALL:
+                case SMALL_THIN:
+                    crustPrice = 4.99;
+                    break;
+                case MEDIUM:
+                case MEDIUM_THIN:
+                    crustPrice = 5.99;
+                    break;
+                case MEDIUM_GF:
+                    crustPrice = 11.99;
+                    break;
+                case LARGE:
+                case LARGE_THIN:
+                    crustPrice = 7.99;
+                    break;
+            }
         }
-        Log.i(TAG, "Generated price: " + price);
-        return price;
+
+
+        Log.i(TAG, "Generated crust price: " + crustPrice);
+        return crustPrice;
     }
 }
